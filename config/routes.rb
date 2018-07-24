9d4f8523c74040db778cef91cbfebfebe4a41d4a
david <daviluqui@gmail.com>
2018-07-24 08:07:48 +0200

    Change default controller name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/welcome'
+  get 'user/welcome'
 
-  root 'pages#welcome'
+  root 'user#welcome'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
