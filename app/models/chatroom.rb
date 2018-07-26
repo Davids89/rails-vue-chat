b8d605c4305ba4773837fc578a7f0c0dfbe47845
david <daviluqui@gmail.com>
2018-07-26 12:02:49 +0200

    Broadcast the model

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -4,4 +4,11 @@ class Chatroom
   field :name, type: String
   
   has_many :messages
+
+  after_create :new_room_broadcast
+
+  private
+  def new_room_broadcast
+    ActionCable.server.broadcast "room_list", room: self
+  end
 end
