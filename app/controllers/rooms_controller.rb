1baa40fc59f66d1b31935236e8f65ca6e60d3d53
david <daviluqui@gmail.com>
2018-07-26 12:01:14 +0200

    Add a method to create a room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,17 @@ class RoomsController < ApplicationController
     @chatroom = Chatroom.all
     render json: @chatroom
   end
+
+  def create
+    @room = Chatroom.new(room_params)
+
+    if @room.save
+      render json: @room
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name)
+  end
 end
