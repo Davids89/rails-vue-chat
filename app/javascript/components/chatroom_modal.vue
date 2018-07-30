a1af6d352be550dcbb3cd6f1f151a690073be0e7
david <daviluqui@gmail.com>
2018-07-30 10:43:21 +0200

    Styling the room list

--- app/javascript/components/chatroom_modal.vue
+++ app/javascript/components/chatroom_modal.vue
@@ -8,8 +8,7 @@
                 </div>
                 <div class="modal-body">
                     <label class="form-label">
-                        Nombre de la sala
-                        <input class="form-control" v-model="chatroom_name">
+                        <input class="form-control" v-model="chatroom_name" placeholder="Nombre de la sala">
                     </label>
                 </div>
                 <div class="modal-footer text-right">
@@ -40,8 +39,10 @@
                 this.$emit('dismiss')
             },
             save: function() {
-                this.$emit('save', this.chatroom_name)
-                this.close()
+                if (this.chatroom_name.length > 0) {
+                    this.$emit('save', this.chatroom_name)
+                    this.close()
+                }
             }
         }
     }
