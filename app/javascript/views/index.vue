1e04087370feb6c747ff8e3062889f17fb169781
david <daviluqui@gmail.com>
2018-07-30 08:49:16 +0200

    Styling the index screen

--- app/javascript/views/index.vue
+++ app/javascript/views/index.vue
@@ -1,18 +1,26 @@
 <template>
     <div class="login-wrapper">
+        <img class="logo" :src=src />
         <div class="input-wrapper">
             <input v-model="username" type="text" placeholder="Usuario" id="username">
             <button v-on:click="login">Entrar</button>
         </div>
+        <p class="login-error" v-if="showError">{{ error }}</p>
     </div>
 </template>
 
 <script>
 
+import logo from '../../assets/images/logo.png'
+
 export default {
+
     data: function() {
         return {
-            username: ''
+            showError: false,
+            error: '',
+            username: '',
+            src: logo
         }
     },
     methods: {
@@ -20,6 +28,17 @@ export default {
             if (this.username.length > 2) {
                 this.$store.commit('login', this.username);
                 this.$router.push('/rooms');
+            } else {
+                this.error = "El nombre debe ser mayor de 2 caracteres"
+                this.showError = true
+            }
+        }
+    },
+    watch: {
+        username: function(newUsername, oldUsername) {
+            this.showError = false
+            if (newUsername.length > 2) {
+                this.disableButton = false
             }
         }
     }
